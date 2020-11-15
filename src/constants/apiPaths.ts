b1ca403efb36e0c2e9cf5e823f38a4f362f4429f
olenastarchak <olenastarchak@ukr.net>
2020-11-15 18:39:13 +0200

    change logic of import files

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://wdzdjsytsh.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://wdzdjsytsh.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://wdzdjsytsh.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://4ze7z45jwj.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://wdzdjsytsh.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
